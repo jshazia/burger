37356ecfd9023d4fef3c158306ae0fd5e987fda4
Shazia Jiwani <jshazia@gmail.com>
2018-05-23 16:53:01 -0400

    jawsdb updated connection to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,17 @@
 var mysql = require("mysql");
-
-var connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "burgers_db"
-});
-
-connection.connect(function(err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-});
-
+var connection;
+if(process.env.JAWSDB_URL) {
+    //Heroku deployment
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    //local host
+    connection = mysql.createConnection({
+        root: 3000,
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "burgers_db",
+    });
+};
 module.exports = connection;
+
